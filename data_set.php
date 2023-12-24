8c789e3643777ea908b837f8c934f11114713dcf
eranda-palliyaguru <erandasampath2000@gmail.com>
2023-12-24 13:27:00 +0530

    update data set php

--- data_set.php
+++ data_set.php
@@ -2,27 +2,21 @@
 session_start();
 include('connect.php');
 
-$result1 = $db->prepare("SELECT * FROM sales ORDER by transaction_id DESC limit 0,500 ");
+$result1 = $db->prepare("SELECT * FROM sales_temp JOIN customer ON sales_temp.cus_id = customer.id ");
 		$result1->bindParam(':userid', $a1);
 		$result1->execute();
 		for($i=0; $row1 = $result1->fetch(); $i++){
-		$job_no=$row1['job_no'];
-		$id=$row1['transaction_id'];
+		$invo=$row1['invo'];
+		$cus_id=$row1['id'];
 		
 		
-$result = $db->prepare("SELECT * FROM job WHERE id='$job_no' ");
-		$result->bindParam(':userid', $a1);
-		$result->execute();
-		for($i=0; $row = $result->fetch(); $i++){
-		$mechanic_id=$row['mechanic_id'];	
-		}
 
 			
 $sql = "UPDATE sales 
-        SET mechanic_id=? 
-		WHERE transaction_id=?";
+        SET customer_id=? 
+		WHERE invoice_number=?";
 $q = $db->prepare($sql);
-$q->execute(array($mechanic_id,$id));		
+$q->execute(array($cus_id,$invo));		
 		
 		}
 
