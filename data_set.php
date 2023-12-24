2e84389abcf21fdfc104eefdb2e48191ae0fa045
eranda-palliyaguru <erandasampath2000@gmail.com>
2023-12-24 13:48:11 +0530

    update

--- data_set.php
+++ data_set.php
@@ -2,19 +2,19 @@
 session_start();
 include('connect.php');
 
-$result1 = $db->prepare("SELECT * FROM sales_temp JOIN customer ON sales_temp.cus_id = customer.cus_id ");
+$result1 = $db->prepare("SELECT * FROM sales JOIN vehicle ON vehicle.customer_id = sales.customer_id ");
 		$result1->bindParam(':userid', $a1);
 		$result1->execute();
 		for($i=0; $row1 = $result1->fetch(); $i++){
-		$invo=$row1['invo'];
+		$invo=$row1['transaction_id '];
 		$cus_id=$row1['id'];
 		
 	echo $cus_id.'--<br>';	
 
 			
 $sql = "UPDATE sales 
-        SET customer_id=? 
-		WHERE invoice_number=?";
+        SET vehicle_id=? 
+		WHERE transaction_id=?";
 $q = $db->prepare($sql);
 $q->execute(array($cus_id,$invo));		
 		
