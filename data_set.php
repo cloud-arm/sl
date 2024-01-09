63831d6d07b67cf666bcd596f20c4263416c0162
eranda-palliyaguru <erandasampath2000@gmail.com>
2024-01-09 12:49:17 +0530

    update

--- data_set.php
+++ data_set.php
@@ -2,27 +2,23 @@
 session_start();
 include('connect.php');
 
-$result1 = $db->prepare("SELECT * FROM sales ");
+$result1 = $db->prepare("SELECT * FROM supplier ");
 		$result1->bindParam(':userid', $a1);
 		$result1->execute();
 		for($i=0; $row1 = $result1->fetch(); $i++){
-		$invo=$row1['transaction_id'];
-		$cus_id=$row1['customer_id'];
-		$vid=0;
+		$invo=$row1['id'];
+		$cus=$row1['old_id'];
 
-		$result = $db->prepare('SELECT * FROM vehicle WHERE  customer_id=:id ');
-		$result->bindParam(':id', $cus_id);
-		$result->execute();
-		for($i=0; $row = $result->fetch(); $i++){ $vid=$row['id']; }
+
 		
-	echo $cus_id.'__'.$vid.'<br>';	
+	echo $cus.'__'.$invo.'<br>';	
 
 			
-$sql = "UPDATE sales 
-        SET vehicle_id=? 
-		WHERE transaction_id=?";
+$sql = "UPDATE purchases 
+        SET supplier_id=? 
+		WHERE suplier_invoice=?";
 $q = $db->prepare($sql);
-$q->execute(array($vid,$invo));		
+$q->execute(array($invo,$cus));		
 		
 		}
 
